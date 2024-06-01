ebeedef4217c9ca80036db58eb0faa8fa4855144
mmsobakin <mmsobakin@spasibosb.ru>
2024-06-01 08:44:49 +0300

    правки под автотест github v7

--- src/service/Manager.java
+++ src/service/Manager.java
@@ -5,11 +5,11 @@ import service.impl.InMemoryTaskManager;
 
 public class Manager {
 
-    public static TaskManager getTaskManager(){
+    public static TaskManager getTaskManager() {
         return new InMemoryTaskManager();
     }
 
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
