fc0293e206ea1129a3dfca011ad456a06990a51b
mmsobakin <mmsobakin@spasibosb.ru>
2024-06-14 17:03:10 +0300

    sprint 7.1

--- src/exceptions/ManagersSaveException.java
+++ src/exceptions/ManagersSaveException.java
@@ -1,9 +1,11 @@
 package exceptions;
 
-public class ManagersSaveException extends RuntimeException{
+public class ManagersSaveException extends RuntimeException {
+    
     public ManagersSaveException() {
         super();
     }
+
     public ManagersSaveException(String message) {
         super(message);
     }
